92c41a6ecaabf042d7b9c2589f6df11ab0465271
Koyote_CL <a.osorio.leiva@gmail.com>
2019-06-07 20:29:58 -0400

    V24
    Version 24

--- mvcProyect/login1.php
+++ mvcProyect/login1.php
@@ -76,23 +76,53 @@
         <!-- Formulario de registro de usuario -->
         <div class="modal-body">
           <div class="col-lg-12">
-            <form id="register-form" action="http://phpoll.com/register/process" method="post" role="form" style="display: block;">
+            <form id="form-registro"  method="post" role="form" style="display: block;">
               <div class="form-group">
-                <input type="text" name="rut" id="rut" tabindex="1" class="form-control" placeholder="Ingrese RUT 12345678-9" value="">
+                <input type="text" name="nombres" id="nombres" tabindex="1" class="form-control" placeholder="Nombres" value="">
               </div>
               <div class="form-group">
-                <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Correo electronico" value="">
+                <input type="text" name="apellido_paterno" id="apellido_paterno" tabindex="2" class="form-control" placeholder="Apellido Paterno" value="">
               </div>
               <div class="form-group">
-                <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Contraseña">
+                <input type="text" name="apellido_materno" id="apellido_materno" tabindex="3" class="form-control" placeholder="Apellido Materno" value="">
+              </div>              
+              <div class="form-group">
+                <select class="form-control" id="tipo_documento" tabindex="4" >
+                  <option>Seleccione tipo de documento</option>
+                  <option>Pasaporte</option>
+                  <option>DNI</option>
+                  <option>RUT</option>
+                </select>
               </div>
               <div class="form-group">
-                <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirmar contraseña">
+                <input type="text" name="documento" id="documento" tabindex="5" class="form-control" placeholder="Ingrese Número Documento" value="">
+              </div>          
+              <div class="form-group">
+                <input type="text" name="direccion" id="direccion" tabindex="6" class="form-control" placeholder="Ingrese Dirección Particular"<value="">
+              </div>
+              <div class="form-group">
+                <select class="form-control" id="comuna" tabindex="7" >
+                  <option>Seleccione Comuna</option>
+                  <option>Rancagua</option>
+                  <option>Codegua</option>
+                  <option>Coinco</option>
+                  <option>Coltauco</option>
+                </select>
+              </div>              
+              <div class="form-group">
+                <input type="email" name="correo" id="correo" tabindex="8" class="form-control" placeholder="Correo electronico" value="">
+              </div>
+              <div class="form-group">
+                <input type="password" name="password" id="password" tabindex="9" class="form-control" placeholder="Contraseña">
+              </div>
+              <div class="form-group">
+                <input type="password" name="confirm-password" id="confirm-password" tabindex="10" class="form-control" placeholder="Confirmar contraseña">
               </div>
               <div class="form-group">
                 <div class="row">
                   <div class="col-sm-6 col-sm-offset-3">
-                    <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn2 btn-success" value="Crear cuenta">
+                    <button type="button" class="btn2 btn btn-success" data-toggle="modal" data-target="#ModalRegistroCorrecto">Crear Cuenta</button>                    
+                    <!-- <input type="submit" name="register-submit" id="register-submit" tabindex="11" class="form-control btn2 btn-success" value="Crear cuenta"> -->
                   </div>
                 </div>
               </div>
@@ -143,7 +173,25 @@
   <!-- ModalRecupera -->  
 
 
-
+  <!-- ModalRegistroCorrecto-->
+  <div class="modal fade" id="ModalRegistroCorrecto">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+      
+        <!-- Cabecera de formulario -->
+        <!-- Formulario de recuperar contraseña mediante correo electrónico -->
+        <div class="modal-body">
+          <div class="col-lg-12">
+            <div class="alert alert-info alert-dismissible">
+              <button type="button" class="close" data-dismiss="alert">&times;</button>
+              <strong>Registro Satisfactorio!</strong> Presione <a href="http://localhost/GitHub/repositorio-kanui/mvcProyect/login1.php" class="alert-link">aquí</a> para continuar.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ModalRecupera -->  
 
 </body>
 </html>
