0b39785180d5d367e85d8c656afff56053f1a201
acandiax <413.candia@gmail.com>
2019-07-12 11:36:08 -0400

    arreglo combobox  regiones / comuna

--- mvcProyect/models/registroUsuariomodel.php
+++ mvcProyect/models/registroUsuariomodel.php
@@ -48,7 +48,7 @@ class RegistroUsuarioModel extends Model{
             $resp = '';
             $rs = mysqli_query($conn,$sql);
             while($row = mysqli_fetch_array($rs)){
-                $resp = "<option value='".$row['id_reg']."'> ".$row['descripcion']."</option>";
+                $resp = "<option value='".$row['id_reg']."'> ".utf8_encode($row['descripcion'])."</option>";
                 echo $resp;
             }
             return $resp;
@@ -67,7 +67,7 @@ class RegistroUsuarioModel extends Model{
             $resp = '';
             $rs = mysqli_query($conn,$sql);
             while($row = mysqli_fetch_array($rs)){
-                $resp = "<option value='".$row['id_com']."'> ".$row['descripcion']."</option>";
+                $resp = "<option value='".$row['id_com']."'> ".utf8_encode($row['descripcion'])."</option>";
                 echo $resp;
             }
             return $resp;
