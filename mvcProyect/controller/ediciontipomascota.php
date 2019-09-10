ffec45e714b872b72544258cf88c2430f5cd73e3
Alfred <a.osorio.leiva@gmail.com>
2019-09-10 19:20:52 -0300

    Pantalla de edicion tipos de mascotas
    Carga pantalla de edición de tipos de mascotas

--- mvcProyect/controller/ediciontipomascota.php
+++ mvcProyect/controller/ediciontipomascota.php
@@ -1,5 +1,5 @@
 <?php
-class ediciontipoMascota extends Controller{
+class ediciontipomascota extends Controller{
 
     function __construct()
     {
@@ -9,18 +9,16 @@ class ediciontipoMascota extends Controller{
     }
     
     function render(){
-        $id_tmasc = $_GET['id_tmasc'];
-        $tipos = [];//aqui creas un arreglo donde puedes guardar los datos
-        //$tipos = $this->model->cargatipomascota($id_tmasc);//los importas desde la funcion
-        $this->view->tipomascota = $respuesta;//creas una variable con la cual podras acceder en la vista
         $this->view->render('ediciontipomascota/ediciontipomascota');
     }
 
     function ediciontipomascota(){
-        $tipo = $_GET['id_tmasc']; 
-        $respuesta = $this->model->cargatipomascota($tipo);
-        return $respuesta;
-        $this->view->render('ediciontipomascota/ediciontipomascota');
+        $id_tmasc = $_GET['id_tmasc']; 
+        $tipos = [];//aqui creas un arreglo donde puedes guardar los datos
+        $tipos = $this->model->cargatipomascota($id_tmasc);//los importas desde la funcion
+        $this->view->tipomascota = $tipos;//creas una variable con la cual podras acceder en la vista
+        $this->view->render('ediciontipomascota/ediciontipomascota');//cargas la vista
+
     }
 
 
--- mvcProyect/views/ediciontipomascota/ediciontipomascota.php
+++ mvcProyect/views/ediciontipomascota/ediciontipomascota.php
@@ -12,22 +12,26 @@
 <div style="padding: 0;padding-right: 21px;">
   <!--<img src="views/imagenes/registro_mascota.png" alt="rdu" style="width:300px;">-->
   <h1>Edicion de Tipos de Mascotas</h1>
-  
+
+                    <?php foreach($this->tipomascota as $r=> $valor): ?>
+                    <?php endforeach; ?>     
+
   <div id="registrar" class="row">
     <div class="col-md-12">
       <div class="well well-sm">
         <form class="form-horizontal" method="post" action="<?php echo constant('URL') ?>ediciontipomascota/ediciontipomascota">
           <div class="container">
             <div class="row">
-              <div class="col-md-6 form-group"><label for="chipID">ID Tipo Mascota</label><br><input id="id_tmasc" readonly name="id_tmasc" type="text" placeholder="ID Tipo Mascota" class="form-control" value="<?php echo $this->tipomascota['id_tmasc'] ?>">
+              <div class="col-md-6 form-group"><label for="ID_TIPO_MASCOTA">ID Tipo Mascota</label><br><input id="id_tmasc" readonly name="id_tmasc" type="text" placeholder="ID Tipo Mascota" class="form-control" value="<?php echo $valor['id_tmasc'];?>">
               </div>
-              <div class="col-md-6 form-group"><label for="nombreM">Descipcion</label><br>
-                <?php echo $this->tipomascota['descripcion'] ?></div>
+              <div class="col-md-6 form-group"><label for="NOMBRE_DESCRIPCION">Descipcion</label><br>
+                <input id="descripcion" name="descripcion" type="text" placeholder="Nombre Tipo Mascota" class="form-control" value="<?php echo $valor['descripcion'];?>">
               </div>
+            </div>
             <div class="row">
               <div class="col-md-8 offset-md-2 form-group" style="text-align:center">
                 <button type="submit" class="btn btn-verde" name="aceptar" style="margin-right:20px">Guardar</button>
-                <button type="button" class="btn btn-verde" data-toggle="modal" data-target="#ModalCancelar">Cancelar</button>
+                <a href='<?php echo constant('URL') ?>listatipomascota' class="btn btn-verde">Cancelar</div></a>
               </div>
             </div>
           </div>
