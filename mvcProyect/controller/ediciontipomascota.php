110f44f72eedc6325512a7d1f99ac7290b360565
DANIEL ALFREDO ZUNIGA BUSTOS <DANIEL.ZUNIGAB@correoaiep.cl>
2019-09-10 22:14:51 -0300

    parche curita

--- mvcProyect/controller/ediciontipomascota.php
+++ mvcProyect/controller/ediciontipomascota.php
@@ -12,7 +12,7 @@ class ediciontipomascota extends Controller{
         $this->view->render('ediciontipomascota/ediciontipomascota');
     }
 
-    function ediciontipomascota(){
+    function ediciontiposmascota(){
         $id_tmasc = $_GET['id_tmasc']; 
         $tipos = [];//aqui creas un arreglo donde puedes guardar los datos
         $tipos = $this->model->cargatipomascota($id_tmasc);//los importas desde la funcion
@@ -25,7 +25,12 @@ class ediciontipomascota extends Controller{
         $id_tmasc = $_POST['id_tmasc'];
         $descripcion = $_POST['txt_desc'];
         $tipos = $this->model->guardatipomascota($id_tmasc,$descripcion);//los importas desde la funcion
-        $this->view->render('listatipomascota/listatipomascota');//cargas la vista
+        //$this->view->render('listatipomascota/listatipomascota');//cargas la vista
+        
+        //PARCHE CURITA
+        echo "<script>window.locatio.href='<?php echoconstant('URL') ?>listatipomascota/listatipomascota ?>'</script>";
+
+        //PARCHE CURITA
 
     }    
 
