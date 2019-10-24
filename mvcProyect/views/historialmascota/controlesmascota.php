43d85041df9faa509d9ad0f226f4070e984cc53d
Alfred <a.osorio.leiva@gmail.com>
2019-10-24 16:57:37 -0300

    Historial de Atenciones

--- mvcProyect/views/historialmascota/controlesmascota.php
+++ mvcProyect/views/historialmascota/controlesmascota.php
@@ -6,7 +6,7 @@
 }    
 </style>
 <div style="padding: 0;padding-right: 21px;">
-    <h1>Historial de Mascotas</h1>
+    <h1>Historial de Atención de Mascotas</h1>
     <div class="container">
         <br>
     </div>
@@ -15,7 +15,7 @@
             <div class="col-md-12" id="resBusqueda" style="height:400px; overflow: auto; border: 1px solid black; padding-left:0;">
                 <table width="100%" style="margin:5px" class="tablaBusqueda table table-striped">
                     <tr>
-                        <th style="width:47px"></th>
+                        <th style="width:47px">Nombre</th>
                         <th>Descripción tipo de mascota</th>
                         <th>Acción</th>
                     </tr>
@@ -23,9 +23,13 @@
                         <!---aqui ibas como avion, como creamos la variable en la vista, y esta es un arreglo, recorremos el arreglo e imprimimos la vista --->
                         <tr>
                             <td><?php echo $valor[0]; ?></td>
-                            <td><?php echo $valor['descripcion']; ?></td>
-                            <td><a href='<?php echo constant('URL') ?>ediciontipomascota/ediciontipomascota?id_tmasc=<?php echo $valor['id_tmasc']; ?>' class="btn btn-verde">EDITAR</div></a>
-                            </td>
+                            <td><?php echo $valor[1]; ?></td>
+                            <td><?php echo $valor[2]; ?></td>
+                            <td><?php echo $valor[3]; ?></td>
+                            <td><?php echo $valor[4]; ?></td>
+                            <td><?php echo $valor[5]; ?></td>
+                            <td><?php echo $valor[6]; ?></td>
+                            <td><?php echo $valor[7]; ?></td>
                         </tr>
                     <?php endforeach; ?>                    
                 </table>
