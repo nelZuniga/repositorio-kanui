450cbba0e264d982f676eb667ac38ce7663b74e6
DANIEL ALFREDO ZUNIGA BUSTOS <DANIEL.ZUNIGAB@correoaiep.cl>
2019-11-04 23:03:31 -0300

    parte 2

--- mvcProyect/views/registrodatosusuario/RegistroDatosUsuario.php
+++ mvcProyect/views/registrodatosusuario/RegistroDatosUsuario.php
@@ -1,4 +1,4 @@
-<?php require 'views/sidemenu.php'?>
+<?php require 'views/sidemenu.php' ?>
 <html lang="en">
 
 <head>
@@ -21,28 +21,33 @@
                 });
             }
             getRegiones();
+
             
-            $("#aceptar").click(function(){
-                event.preventDefault();
-                if(comprobar()){
-                    console.log("comprueba")
-                    $("#nuevousuario").attr('action',"<?php echo constant('URL') ?>registroUsuario/nuevoUsuario");
-                    $("#nuevousuario").submit();
-                }
-            });
         }); //fin document ready
 
-        function comprobar(){
+        function comprobar() {
             var comprobado = false;
             var compass = false;
             var valorpass = $("#pass").val();
             var valorpass2 = $("#pass").val();
-            if( valorpass == valorpass2){
+            if (valorpass == valorpass2) {
                 compass = true
             }
-            if(compass){
-                comprobado = true;
+            console.log(compass);
+            if(!comprobado){
+                Swal.fire(
+                            'Atencion',
+                            'Las contraseñas no coinciden',
+                            'error'
+                        ).then(function(){
+                            $("#pass2").val('')
+                            $("#pass2").addClass('is-invalid');
+                        })
+            }else{
+                $("#pass2").removeClass('is-invalid');
+                $("#pass2").addClass('is-valid');
             }
+            console.log(comprobado);
             return comprobado;
         }
 
@@ -69,22 +74,22 @@
             });
         }
 
-            function getRol() {
-                var url = "<?php echo constant('URL') ?>registroUsuario/getRol";
-                $.ajax({
-                    url: url,
-                    success: function(data) {
-                        console.log(data);
-                        $("#rol_id").empty();
-                        $("#rol_id").append("<option value=''>Seleccione un Rol de Usuario</option>");
-                        $("#rol_id").append(data);
-                    },
-                    error: function() {
-                        alert("error");
-                    }
-                });
-            }
-            getRol();       
+        function getRol() {
+            var url = "<?php echo constant('URL') ?>registroUsuario/getRol";
+            $.ajax({
+                url: url,
+                success: function(data) {
+                    console.log(data);
+                    $("#rol_id").empty();
+                    $("#rol_id").append("<option value=''>Seleccione un Rol de Usuario</option>");
+                    $("#rol_id").append(data);
+                },
+                error: function() {
+                    alert("error");
+                }
+            });
+        }
+        getRol();
 
         var numeros = "0123456789";
 
@@ -153,44 +158,86 @@
                     }
                 }
             }
-            if(seguridad <= 10){
+            if (seguridad <= 10) {
                 $("#seguridad").empty();
                 $("#seguridad").append("muy leve");
                 $("#pass2").attr("readonly", true)
-            }else if(seguridad <= 30){
+            } else if (seguridad <= 30) {
                 $("#seguridad").empty();
                 $("#seguridad").append("leve");
                 $("#pass2").attr("readonly", true)
-            }else if(seguridad <= 39){
+            } else if (seguridad <= 39) {
                 $("#seguridad").empty();
                 $("#seguridad").append("moderada");
                 $("#pass2").attr("readonly", false)
-            }else if(seguridad <= 59){
+            } else if (seguridad <= 59) {
                 $("#seguridad").empty();
                 $("#seguridad").append("Buena");
                 $("#pass2").attr("readonly", false)
-            }else if(seguridad >= 60){
+            } else if (seguridad >= 60) {
                 $("#seguridad").empty();
                 $("#seguridad").append("Muy Buena");
                 $("#pass2").attr("readonly", false)
             }
             return seguridad
         }
+
+        function checkCorreo(caja) {
+            if(caja.value.length == 0 || caja.value== ''){
+
+            }else{
+            var url = "<?php echo constant('URL') ?>registroUsuario/checkCorreo";
+            var mail = caja.value;
+            var parametrosajax = {
+                mail: mail
+            }
+            $.ajax({
+                url: url,
+                data: parametrosajax,
+                type: 'post',
+                success: function(data) {
+                    if (data != 0) {
+                        Swal.fire(
+                            'Atencion',
+                            'El correo ya se encuentra registrado el en sistema',
+                            'error'
+                        ).then(function(){
+                            $("#correo").focus();
+                            //$("#correo").val('');
+                            $("#correo").removeClass('is-valid');
+                        $("#correo").addClass('is-invalid');
+                        })
+                            
+
+                    }else{
+                        $("#correo").removeClass('is-invalid');
+                        $("#correo").addClass('is-valid');
+                    }
+                },
+                error: function() {
+                    alert("error");
+                }
+            });
+            }
+        }
     </script>
 </head>
 
 
 <body>
+
     <body>
         <div class="container">
             <h2>Registro de Usuarios</h2>
             <div class="row">
                 <div class="col-md-12"></div>
                 <div class="col-md-12">
-                    <form method="POST" onsubmit="comprobar()" name="nuevousuario" id="nuevousuario">
-                    <input type="hidden" name="tusr" value="2">
+                    <form method="POST" onsubmit="comprobar()" name="nuevousuario" id="nuevousuario" action="<?php echo constant('URL') ?>registroUsuario/nuevoUsuario">
+                        <input type="hidden" name="tusr" value="2">
                         <div class="form-group col-md-12">
-                            <!-- nombres --><!-- apellido paterno --><!-- apellido materno -->
+                            <!-- nombres -->
+                            <!-- apellido paterno -->
+                            <!-- apellido materno -->
                             <div class="row col-md-12">
                                 <label for="nombres_id" class="control-label col-md-3">Nombres</label>
                                 <label for="espaciados" class="control-label col-md-1"></label>
@@ -216,23 +263,25 @@
                                 <input type="text" class="form-control col-md-4 rut" id="txtrut" name="Drut" placeholder="Ingrese Rut Ej. 11222333k" pattern="\d{3,8}-[\d|kK]{1}" required>
                                 <label for="espaciados" class="control-label col-md-1"></label>
                                 <select class="form-control col-md-4" id="rol_id" name="rol_id" required>
-                                <option value=''>Seleccione un Rol de Usuario</option>
-                                </select> 
+                                    <option value=''>Seleccione un Rol de Usuario</option>
+                                </select>
                             </div>
                             <BR>
-                            <!-- Correo --><!-- telefono -->
+                            <!-- Correo -->
+                            <!-- telefono -->
                             <div class="row col-md-12">
                                 <label for="correo" class="control-label col-md-4">Correo</label>
                                 <label for="espaciados" class="control-label col-md-1"></label>
                                 <label for="telefono_id" class="control-label col-md-4">Telefono</label>
                             </div>
                             <div class="row col-md-12">
-                                <input type="email" class="form-control col-md-4" id="correo" name="correo" placeholder="Ingese su E-mail" required>
+                                <input type="email" class="form-control col-md-4" id="correo" name="correo" placeholder="Ingese su E-mail" onblur="checkCorreo(this)" required>
                                 <label for="espaciados" class="control-label col-md-1"></label>
                                 <input type="text" class="form-control numeros col-md-4" id="txttelefono" name="Dtelefono" placeholder="988888888" required>
                             </div>
                             <BR>
-                            <!-- direccion--><!-- cuidad-->
+                            <!-- direccion-->
+                            <!-- cuidad-->
                             <div class="row col-md-12">
                                 <label for="street2_id" class="control-label col-md-5">Direccion</label>
                                 <label for="espaciados" class="control-label col-md-1"></label>
@@ -244,7 +293,8 @@
                                 <input type="text" class="form-control col-md-5" id="txtciudad" name="Vciudad" placeholder="Ingrese su ciudad" required>
                             </div>
                             <BR>
-                            <!-- combo region --><!-- combo comuna -->
+                            <!-- combo region -->
+                            <!-- combo comuna -->
                             <div class="row col-md-12">
                                 <label for="region_id" class="control-label col-md-5" id="region" name="region">Región</label>
                                 <label for="espaciados" class="control-label col-md-1"></label>
@@ -255,11 +305,12 @@
                                 </select>
                                 <label for="espaciados" class="control-label col-md-1"></label>
                                 <select class="form-control col-md-5" id="comuna_id" name="comuna_id" required>
-                                <option value=''>Seleccione Una Comuna</option>
-                                </select>                            
+                                    <option value=''>Seleccione Una Comuna</option>
+                                </select>
                             </div>
                             <BR>
-                            <!-- contraseña--><!-- repetir contraseña-->
+                            <!-- contraseña-->
+                            <!-- repetir contraseña-->
                             <div class="row col-md-12">
                                 <label for="pass2" class="control-label col-md-5">Contraseña</label>
                                 <label for="espaciados" class="control-label col-md-1"></label>
@@ -268,12 +319,12 @@
                             <div class="row col-md-12">
                                 <input type="password" class="form-control col-md-5" id="pass" name="pass" placeholder="Cree su contraseña" onkeyup="seguridad_clave(this)" required>
                                 <label for="espaciados" class="control-label col-md-1"></label>
-                                <input type="password" class="form-control col-md-5" id="pass2" name="pass2" placeholder="Repita contraseña" readonly required>
+                                <input type="password" class="form-control col-md-5" id="pass2" name="pass2" placeholder="Repita contraseña" onblur=comprobar() readonly required>
                             </div>
                             <BR>
                             <div class="row col-md-12">
                                 <h4>
-                                Nivel de seguridad de contraseña: <span id="seguridad"></span>
+                                    Nivel de seguridad de contraseña: <span id="seguridad"></span>
                                 </h4>
                             </div>
                         </div>
@@ -283,20 +334,20 @@
                                 <th>
                                     <div class="col-md-12" align="center">
                                         <button class="btn btn-verde" id="aceptar" name="aceptar">Aceptar</button>
-                                        
+
                                     </div>
                                 </th>
                                 <th>
                                 </th>
                                 <th>
                                     <div class="col-md-12" align="center">
-                                        <button class="btn btn-verde">Cancelar</button>   
+                                        <button class="btn btn-verde">Cancelar</button>
                                     </div>
                                 </th>
                             </tr>
                         </table>
-                    </div>
-                    </form>
+                </div>
+                </form>
             </div>
 
 
